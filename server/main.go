9facfcd526d50483871d379aeaa68b7fd6bb9743
913x100 <kittinut.iam@gmail.com>
2021-04-19 00:23:34 +0700

    add expense api

--- server/main.go
+++ server/main.go
@@ -30,12 +30,20 @@ func main() {
 	})
 	e.POST("/income", AddIncome)
 	e.GET("/income", GetIncome)
+	e.POST("/expense", AddExpense)
+	e.GET("/expense", GetExpense)
 
 	e.Logger.Fatal(e.Start(":3002"))
 	
 }
 
 type Income struct {
+	ID string `json:"id"`
+	Text string `json:"text"`
+	Amount int `json:"amount"`
+}
+
+type Expense struct {
 	ID string `json:"id"`
 	Text string `json:"text"`
 	Amount int `json:"amount"`
@@ -70,4 +78,35 @@ func GetIncome(c echo.Context) error {
 	db.Find(&income)
 
 	return c.JSON(http.StatusOK, income)
+}
+
+func AddExpense(c echo.Context) error {
+	var expense Expense
+	if err := c.Bind(&expense); err != nil {
+		log.Error(err)
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		log.Error(err)
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	db.Create(&expense)
+
+	return c.String(http.StatusOK, "add success")
+}
+
+func GetExpense(c echo.Context) error {
+	var expense []Expense
+
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	db.Find(&expense)
+
+	return c.JSON(http.StatusOK, expense)
 }
