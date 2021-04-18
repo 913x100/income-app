de0dc34e2117ddc2798c595cf3280ac92bde1ef0
913x100 <kittinut.iam@gmail.com>
2021-04-18 22:45:51 +0700

    add gitignore

--- server/main.go
+++ server/main.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -20,4 +22,40 @@ func main() {
 
 	e.Logger.Fatal(e.Start(":3002"))
 	
+}
+
+type Income struct {
+	ID string `json:"id"`
+	Text string `json:"text"`
+	Amount int `json:"amount"`
+}
+
+func AddIncome(c echo.Context) error {
+	var income Income
+
+	if err := c.Bind(income); err != nil {
+		return err
+	}
+
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "there are errors")
+	}
+
+	db.Create(&income)
+
+	return c.String(http.StatusOK, "add success")
+}
+
+func GetIncome(c echo.Context) error {
+	var incomes []Income
+
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	db.Find(&incomes)
+
+	return c.JSON(http.StatusOK, incomes)
 }
